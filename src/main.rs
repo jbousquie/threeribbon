6f03c5325d5a4838e6c00f336e6407064bdb58fd
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2024-12-09 16:59:23 +0100

    test UVs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+// Entry point for non-wasm
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
+async fn main() {
+    run().await;
+}
+
 use three_d::*;
 
-pub fn main() {
+pub async fn run() {
     let window = Window::new(WindowSettings {
         title: "Shapes!".to_string(),
         max_size: Some((1280, 720)),
@@ -11,8 +18,8 @@ pub fn main() {
 
     let mut camera = Camera::new_perspective(
         window.viewport(),
-        vec3(5.0, 2.0, 2.5),
-        vec3(0.0, 0.0, -0.5),
+        vec3(0.0, 0.0, 10.0),
+        vec3(0.0, 0.0, 0.0),
         vec3(0.0, 1.0, 0.0),
         degrees(45.0),
         0.1,
@@ -22,31 +29,39 @@ pub fn main() {
     let light0 = DirectionalLight::new(&context, 1.0, Srgba::WHITE, vec3(0.0, -0.5, -0.5));
     let light1 = DirectionalLight::new(&context, 1.0, Srgba::WHITE, vec3(0.0, 0.5, 0.5));
 
+
+    let mut loaded = three_d_asset::io::load_async(&["assets/checkerboard.jpg"]).await.unwrap();
+    let mut cpu_texture: CpuTexture = loaded.deserialize("checkerboard").unwrap();
+    // cpu_texture.wrap_s = Wrapping::Repeat;
+    // cpu_texture.wrap_t = Wrapping::Repeat;
+    cpu_texture.data.to_color();
     let cpu_material = CpuMaterial {
-        albedo: Srgba { r: 255, g: 0, b: 0, a: 200, },
+        albedo: Srgba { r: 220, g: 220, b: 220, a: 200, },
+        albedo_texture: Some(cpu_texture),
         ..Default::default()
     };
     let material = PhysicalMaterial::new_transparent(&context, &cpu_material);
 
-    // Geometry
-    let cpu_mesh = CpuMesh {
-        positions: Positions::F32( vec![
-            vec3(0.0, 0.0, 0.0),
-            vec3(1.0, 0.0, 0.0),
-            vec3(0.0, 1.0, 0.0),
-            vec3(1.0, 1.0, 0.0),
-        ]),
-        indices: Indices::U8(vec![0, 1, 2, 1, 3, 2]),
-        ..Default::default()
-    };
+
+
+    let mut path_array= Vec::new();
+    for i in -100..100 {
+        let mut path = Vec::new();
+        for j in -40..40 {
+            let z = ((i as f32 + j as f32) * 0.1).sin();
+            let s = 0.2;
+            path.push(vec3(i as f32 * s, j as f32 * s, z));
+        }
+        path_array.push(path);
+    }
+
+    let ribbon = cpu_ribbon(&path_array);
 
     // Mesh
     let mesh = Gm::new(
-        Mesh::new(&context, &cpu_mesh),
+        Mesh::new(&context, &ribbon),
         material,
     );
-
-
 
     window.render_loop(move |mut frame_input| {
         camera.set_viewport(frame_input.viewport);
@@ -54,7 +69,7 @@ pub fn main() {
 
         frame_input
             .screen()
-            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+            .clear(ClearState::color_and_depth(0.1, 0.1, 0.1, 1.0, 1.0))
             .render(
                 &camera,
                 &mesh,
@@ -63,4 +78,79 @@ pub fn main() {
 
         FrameOutput::default()
     });
+}
+
+
+
+
+fn cpu_ribbon(path_array: &Vec<Vec<Vec3>>) -> CpuMesh {
+    // path lengths
+    let p = path_array.len();
+    let l = path_array[0].len();
+
+    // vertex data arrays
+    let mut positions = Vec::new();
+    let mut indices: Vec<u32> = Vec::new();
+    let mut uvs = Vec::new();
+
+    // variables for uv mapping
+    let mut u_distance = vec![0.0; l];
+    let mut v_distance = vec![0.0; p];
+    let mut u_total_distance = 0.0;
+    let mut v_total_distance = 0.0;
+
+    // positions
+    for i in 0..p {
+        for j in 0..l {
+            let v3 = path_array[i][j].clone();
+            positions.push(v3);
+            if j > 0 {
+                u_total_distance += (path_array[i][j] - path_array[i][j - 1]).magnitude();
+                u_distance[j] = u_total_distance;
+            }
+            if i > 0 {
+                v_total_distance += (path_array[i][j] - path_array[i - 1][j]).magnitude();
+                v_distance[i] = v_total_distance;
+            }
+        }
+    }
+
+    // uvs
+    for i in 0..p {
+        for j in 0..l {
+            uvs.push(vec2(
+                u_distance[j] / u_total_distance,
+                v_distance[i] / v_total_distance,
+
+            ));
+        }
+    }
+     
+    // indices
+    for i in 0..p - 1 {
+        for j in 0..l - 1 {
+            let i0 = i * l + j;
+            let i1 = i * l + j + 1;
+            let j0 = (i + 1) * l + j;
+            let j1 = (i + 1) * l + j + 1;
+
+            indices.push(i0 as u32);
+            indices.push(i1 as u32);
+            indices.push(j1 as u32);
+
+            indices.push(j1 as u32);
+            indices.push(j0 as u32);
+            indices.push(i0 as u32);
+        }
+    }
+
+    let mut cpu_mesh = CpuMesh {
+        positions: Positions::F32(positions),
+        indices: Indices::U32(indices),
+        uvs: Some(uvs),
+        ..Default::default()
+    };
+    cpu_mesh.compute_normals();
+    cpu_mesh.compute_tangents();
+    cpu_mesh
 }
